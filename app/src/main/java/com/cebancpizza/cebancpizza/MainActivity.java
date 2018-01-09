a731d2ea034f6df8f3ae2436e53bfb64ebbb78e3
gorka <pivot32@hotmail.com>
2018-01-09 12:04:15 +0100

    a

--- app/src/main/java/com/cebancpizza/cebancpizza/MainActivity.java
+++ app/src/main/java/com/cebancpizza/cebancpizza/MainActivity.java
@@ -2,6 +2,8 @@ package com.cebancpizza.cebancpizza;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void pedirCliente(View v){
+        Intent intent= new Intent(this, datosUsuario.class);
+        startActivity(intent);
 
     }
 }
