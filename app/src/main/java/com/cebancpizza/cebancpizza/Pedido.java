86e3c47d3acedd0cf030e04ba228b101bf4c7934
gorka <pivot32@hotmail.com>
2018-01-10 19:59:31 +0100

    asd

---
+++ app/src/main/java/com/cebancpizza/cebancpizza/Pedido.java
@@ -0,0 +1,14 @@
+package com.cebancpizza.cebancpizza;
+
+/**
+ * Created by gorka on 10/01/2018.
+ */
+
+public class Pedido {
+    Usuario usuario;
+
+    public Pedido(Usuario usuario){
+        this.usuario=usuario;
+    }
+
+}
