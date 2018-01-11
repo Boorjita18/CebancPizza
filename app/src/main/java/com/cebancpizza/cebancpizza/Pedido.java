4668d6889d7f8ee68c2699a582c1299bb117ce26
gorka <pivot32@hotmail.com>
2018-01-11 12:35:00 +0100

    a

---
+++ app/src/main/java/com/cebancpizza/cebancpizza/Pedido.java
@@ -0,0 +1,18 @@
+package com.cebancpizza.cebancpizza;
+
+/**
+ * Created by adminportatil on 11/01/2018.
+ */
+
+public class Pedido extends android.app.Application {
+
+    private Usuario usuario;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuairo(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
---
+++ app/src/main/java/com/cebancpizza/cebancpizza/Usuario.java
@@ -0,0 +1,45 @@
+package com.cebancpizza.cebancpizza;
+
+/**
+ * Created by adminportatil on 11/01/2018.
+ */
+
+public class Usuario {
+    String nombre;
+    String apellidos;
+    int telefono;
+    String direccion;
+    String email;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
