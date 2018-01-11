8c0ee95e3f40947a9c87ff5ebae4cdf5364b6d53
gorka <pivot32@hotmail.com>
2018-01-11 20:54:28 +0100

    asd

--- app/src/main/java/com/cebancpizza/cebancpizza/Bebida.java
+++ app/src/main/java/com/cebancpizza/cebancpizza/Bebida.java
@@ -8,10 +8,29 @@ public class Bebida {
 
     String nombre;
     int cantidad;
+    double precio;
 
-    public Bebida(String nombre,int cantidad){
+    public Bebida(String nombre,int cantidad,double precio){
         this.nombre= nombre;
         this.cantidad=cantidad;
-
+        this.precio=precio;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+    public int getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
     }
 }
--- app/src/main/java/com/cebancpizza/cebancpizza/Postre.java
+++ app/src/main/java/com/cebancpizza/cebancpizza/Postre.java
@@ -8,13 +8,31 @@ public class Postre {
 
         String nombre;
         int cantidad;
+        double precio;
 
 
-        public Postre(String nombre,int cantidad){
+        public Postre(String nombre,int cantidad ,double precio){
             this.nombre= nombre;
             this.cantidad=cantidad;
-
+            this.precio=precio;
         }
-
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+    public int getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 
 }
