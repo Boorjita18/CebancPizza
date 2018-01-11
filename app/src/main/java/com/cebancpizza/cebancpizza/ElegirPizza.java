82b4eb1208b36d3cf8c2aa8d6016a088dfc37ab4
Aitor <aitoretxeza@gmail.com>
2018-01-11 14:24:54 +0100

    aaa

--- app/src/main/java/com/cebancpizza/cebancpizza/ElegirPizza.java
+++ app/src/main/java/com/cebancpizza/cebancpizza/ElegirPizza.java
@@ -1,7 +1,9 @@
 package com.cebancpizza.cebancpizza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class ElegirPizza extends AppCompatActivity {
@@ -12,5 +14,13 @@ public class ElegirPizza extends AppCompatActivity {
         setContentView(R.layout.activity_elegir_pizza);
         Toast.makeText(this, ((Pedido) this.getApplication()).getUsuario().getNombre()+"\n" + ((Pedido) this.getApplication()).getUsuario().getDireccion(),
                 Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void anadirBarbacoa(View v){
+            Intent intent = new Intent(this, opcionesPizza.class);
+            intent.putExtra("Nombre", "Barbacoa");
+            intent.putExtra("Precio", "5,00");
+            startActivity(intent);
     }
 }
